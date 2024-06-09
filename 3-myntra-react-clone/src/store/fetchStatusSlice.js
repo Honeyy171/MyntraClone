a1d6078e0f16e02f1a24a3056d1d058aa562153a
Honeyy171 <hunnygoyal2011@gmail.com>
2024-06-09 13:03:08 +0530

    MyntraClone

---
+++ 3-myntra-react-clone/src/store/fetchStatusSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const  fetchStatusSlice =  createSlice({
+  name: 'fetchStatus',
+  initialState: {
+    fetchDone: false, // false: 'Pending' and true: 'Done'
+    currentlyFetching: false,
+  },
+  reducers: {
+    markFetchDone: (state) => 
+       state.fetchDone = true,
+    
+    markFetchStarted : (state) => 
+       state.currentlyFetching = true,
+    
+    markFetchingFinished: (state) => 
+       state.currentlyFetching = false,
+    
+  }
+
+});
+
+
+export const  fetchStatusActions = fetchStatusSlice.actions;
+
+export default fetchStatusSlice;
